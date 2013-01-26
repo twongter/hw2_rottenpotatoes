a7c1b28ecc40f4d0231613fdbf08ea7c83679ba2
twongter <twongter@gmail.com>
2013-01-25 18:58:42 -0800

    r3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,14 +7,31 @@ class MoviesController < ApplicationController
   end
 
   def index
+  #list of required params, do redirect to make it RESTful
+  req_params = { #{key => def_value, ...
+    :sort_by => nil,
+    :ratings => Hash[Movie.all_ratings.map {|r| [r, "1"]}]}
+  params_val = {}
+  tobe_redirect = false
+  req_params.each do |k,v|
+    if !params[k] && session[k]
+      #in case of missing params but seesion of last value is found
+      tobe_redirect = true
+    else
+      session[k] = (params[k] or v)
+    end
+    params_val[k] = session[k]
+  end
+  if tobe_redirect then
+    flash.keep
+    redirect_to movies_path(params_val)
+  end
   #sorting issue
-    sorting_attr = (params[:sort_by] or session[:sort_by])
-    session[:sort_by] = sorting_attr #keep the latest setting
+    sorting_attr = session[:sort_by]
     eval "@sort_by_#{sorting_attr} = 1"
   #rating issue
+    @selected_ratings = session[:ratings]
     @all_ratings = Movie.all_ratings
-    @selected_ratings = (params[:ratings] or session[:ratings] or Hash[@all_ratings.map {|r| [r, "1"]}])
-    session[:ratings] = @selected_ratings
   #get the movies
     @movies = Movie.order(sorting_attr).find_all_by_rating(@selected_ratings.keys)
   end
